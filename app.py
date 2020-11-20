c9e70383ffafab99b271b607164e068c66247863
virtualspark <stanley.tran@mail.mcgill.ca>
2020-11-20 15:34:08 -0500

    Error Page added

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.errorhandler(500)
+def page_not_found(e):
+   return render_template('error_page.html', title='500'), 500
 
 @app.route('/', methods=['GET'])
 def hello():
